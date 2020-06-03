6b1bd72100e260c1379291da060b3f3ae05e1da5
Almat <almatrakh97@gmail.com>
2020-06-03 23:26:33 +0600

    Configured Api info

--- src/main/java/kz/almat/swaggerpractice/SwaggerPracticeApplication.java
+++ src/main/java/kz/almat/swaggerpractice/SwaggerPracticeApplication.java
@@ -5,9 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -18,12 +22,25 @@ public class SwaggerPracticeApplication {
     }
 
     @Bean
-    public Docket swaggerConfiguration(){
+    public Docket swaggerConfiguration() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .paths(PathSelectors.ant("/api/*"))
                 .apis(RequestHandlerSelectors.basePackage("kz.almat"))
-                .build();
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Swagger Practice API",
+                "Example",
+                "1.0",
+                "Free to use",
+                new Contact("Almat Rakhmetolla", "@rkalmat", "almat.rakhmetolla@gmail.com"),
+                "API License",
+                "rkalmat.com",
+                Collections.emptyList());
     }
 
 }
